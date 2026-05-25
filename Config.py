62044cc3367ce2f97d87e2e8258f1989e05a84c6
cagourav0777-source <cagourav0777@gmail.com>
2026-05-25 08:24:50 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,18 +3,18 @@ import os
 class Config():
   ENV = bool(os.environ.get('ENV', False))
   if ENV:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8978746404:AAFpo-8ohELHLn30J_FLJPv3Kmepm093ZBY")
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    APP_ID = os.environ.get("APP_ID", 6)
-    API_HASH = os.environ.get("API_HASH", None)
+    APP_ID = os.environ.get("APP_ID", "32567356")
+    API_HASH = os.environ.get("API_HASH", "89abc80e49927ea99c552f95af4716a0")
     SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8978746404:AAFpo-8ohELHLn30J_FLJPv3Kmepm093ZBY"
     DATABASE_URL = ""
-    APP_ID = ""
-    API_HASH = ""
+    APP_ID = "32567356"
+    API_HASH = "89abc80e49927ea99c552f95af4716a0"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
